974450e5068d748a0ef2e9432d66e167e6beaa6f
fanzheng <montage_fz@sina.com>
2019-07-18 13:23:38 +0800

    - [新增] 在使用prettier  的时候, 与vue-cli3.0配合,  需要自己添加.prettierrc文件

--- big-data-web/.eslintrc.js
+++ big-data-web/.eslintrc.js
@@ -1,14 +1,16 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  },
-  parserOptions: {
-    parser: "babel-eslint"
-  }
+    root: true,
+    env: {
+        node: true
+    },
+    extends: ['plugin:vue/essential', '@vue/prettier'],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'prettier/prettier': 'error',
+        'quotes': [1, 'single']
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    }
 };
