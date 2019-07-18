2f1a46994f95c5c356d57b3de1b8db7d0902d3f1
fanzheng <montage_fz@sina.com>
2019-07-18 17:11:03 +0800

    - [新增] 添加格式化文件

--- big-data-web/.eslintrc.js
+++ big-data-web/.eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'prettier/prettier': 'error',
-        'quotes': [1, 'single']
     },
     parserOptions: {
         parser: 'babel-eslint'
